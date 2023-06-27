cc4653ef4ea22d69a535599af8a1f20c5657e36e
emerson espinoza <xinayespinoza@gmail.com>
2023-06-26 21:32:12 -0400

    primer commit

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+# creando nuestra calculadora con versionamiento 
+def sumar(a, b):
+    return a+b
+def restar(x, y):
+    return x-y 
+
+# imprimiendo e invocando 
+print("la suma es: ", sumar(30, 50))
+print("la resta es: ", restar(60, 20))
